80e00ba61d2875485997d6271bc6cf4e78440ceb
Sean Warne <swarne@pop-os.localdomain>
2024-01-09 22:01:33 -0700

    added clamping

--- game_1/scripts/player1.gd
+++ game_1/scripts/player1.gd
@@ -3,10 +3,12 @@ extends CharacterBody2D
 const SPEED = 400.0
 
 func _physics_process(delta):
+	get_input()
+	move_and_slide()
+
+func get_input():
 	var direction = Input.get_axis("Player1Up", "Player1Down")
 	if direction:
 		velocity.y = direction * SPEED
 	else:
 		velocity.y = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
--- game_1/scripts/player2.gd
+++ game_1/scripts/player2.gd
@@ -1,12 +1,22 @@
 extends CharacterBody2D
 
-const SPEED = 400.0
+const SPEED = 600.0
+const clamp_vector = Vector2(35, 35)
+
+@onready var screen_size = get_viewport_rect().size
+
+func _ready():
+	pass
 
 func _physics_process(delta):
+	get_input()
+	move_and_slide()
+
+func get_input():
 	var direction = Input.get_axis("Player2Up", "Player2Down")
 	if direction:
 		velocity.y = direction * SPEED
 	else:
 		velocity.y = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+		
+	position = position.clamp(clamp_vector, screen_size - clamp_vector)
